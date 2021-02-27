2d2b36b6e52f1ac507529ebbcc93968ff6894160
Naoya Okanami <minaminaoy@gmail.com>
2021-02-27 18:56:17 +0900

    Add Finalized Blocks Page (#22)

---
+++ dashboard/src/views/finalized_blocks/FinalizedBlockListView/Toolbar.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Box,
+  Button,
+  makeStyles
+} from '@material-ui/core';
+// import { Search as SearchIcon } from 'react-feather';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  importButton: {
+    marginRight: theme.spacing(1)
+  },
+  exportButton: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const Toolbar = ({ className, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+      >
+        <Button className={classes.importButton}>
+          Import
+        </Button>
+        <Button className={classes.exportButton}>
+          Export
+        </Button>
+      </Box>
+      {/* <Box mt={3}>
+        <Card>
+          <CardContent>
+            <Box maxWidth={500}>
+              <TextField
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SvgIcon
+                        fontSize="small"
+                        color="action"
+                      >
+                        <SearchIcon />
+                      </SvgIcon>
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="Search block"
+                variant="outlined"
+              />
+            </Box>
+          </CardContent>
+        </Card>
+      </Box> */}
+    </div>
+  );
+};
+
+Toolbar.propTypes = {
+  className: PropTypes.string
+};
+
+export default Toolbar;
---
+++ dashboard/src/views/finalized_blocks/FinalizedBlockListView/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Container,
+  Grid,
+  makeStyles,
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import Results from './Results';
+// import Toolbar from './Toolbar';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const BlockListView = () => {
+  const classes = useStyles();
+  const [blocks, setBlocks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:' + process.env.REACT_APP_PORT_NUMBER + '/beacon/finalized_blocks'
+      );
+
+      let blocks = result.data;
+      setBlocks(blocks);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Page
+      className={classes.root}
+      title="Blocks"
+    >
+      <Grid>
+        <Container maxWidth={false}>
+          {/* <Toolbar /> */}
+          <Box mt={3}>
+            <Results blocks={blocks} />
+          </Box>
+        </Container>
+      </Grid>
+    </Page>
+  );
+};
+
+export default BlockListView;
